cdd9e0d4972365637a82d496a8626319113193ae
leminhduc1202 <duc.leminh.dn@gmail.com>
2021-08-12 15:07:48 +0700

    Update code

--- app/src/main/java/mdideas/devapp/calculatorbasic/MainActivity.kt
+++ app/src/main/java/mdideas/devapp/calculatorbasic/MainActivity.kt
@@ -2,7 +2,11 @@ package mdideas.devapp.calculatorbasic
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import mdideas.devapp.calculatorbasic.databinding.ActivityMainBinding
+import org.mariuszgromada.math.mxparser.Expression
+import java.text.DecimalFormat
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -13,31 +17,89 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.buttonPlus.setOnClickListener {
-            var number01 : Int = (binding.firstNumber.text.toString().toInt())
-            var number02 : Int = (binding.secondNumber.text.toString().toInt())
-            var result : Int = number01 + number02
-            binding.result.text = result.toString()
+        binding.buttonClear.setOnClickListener {
+            binding.input.text = " "
+            binding.input.text = " "
         }
-        binding.buttonPlus.setOnClickListener {
-            var number01 : Int = (binding.firstNumber.text.toString().toInt())
-            var number02 : Int = (binding.secondNumber.text.toString().toInt())
-            var result : Int = number01 - number02
-            binding.result.text = result.toString()
+        binding.buttonBracketLeft.setOnClickListener {
+            binding.input.text = addToInputText("(")
         }
-        binding.buttonPlus.setOnClickListener {
-            var number01 : Int = (binding.firstNumber.text.toString().toInt())
-            var number02 : Int = (binding.secondNumber.text.toString().toInt())
-            var result : Int = number01 * number02
-            binding.result.text = result.toString()
+        binding.buttonBracketRight.setOnClickListener {
+            binding.input.text = addToInputText(")")
         }
-        binding.buttonPlus.setOnClickListener {
-            var number01: Int = (binding.firstNumber.text.toString().toInt())
-            var number02: Int = (binding.secondNumber.text.toString().toInt())
-            var result: Int = number01 / number02
-            binding.result.text = result.toString()
+        binding.button0.setOnClickListener {
+            binding.input.text = addToInputText("0")
         }
-
+        binding.button1.setOnClickListener {
+            binding.input.text = addToInputText("1")
+        }
+        binding.button2.setOnClickListener {
+            binding.input.text = addToInputText("2")
+        }
+        binding.button3.setOnClickListener {
+            binding.input.text = addToInputText("3")
+        }
+        binding.button4.setOnClickListener {
+            binding.input.text = addToInputText("4")
+        }
+        binding.button5.setOnClickListener {
+            binding.input.text = addToInputText("5")
+        }
+        binding.button6.setOnClickListener {
+            binding.input.text = addToInputText("6")
+        }
+        binding.button7.setOnClickListener {
+            binding.input.text = addToInputText("7")
+        }
+        binding.button8.setOnClickListener {
+            binding.input.text = addToInputText("8")
+        }
+        binding.button9.setOnClickListener {
+            binding.input.text = addToInputText("9")
+        }
+        binding.buttonDot.setOnClickListener {
+            binding.input.text = addToInputText(".")
+        }
+        binding.buttonAddition.setOnClickListener {
+            binding.input.text = addToInputText("+")
+        }
+        binding.buttonSubtraction.setOnClickListener {
+            binding.input.text = addToInputText("-")
+        }
+        binding.buttonMultiply.setOnClickListener {
+            binding.input.text = addToInputText("×")
+        }
+        binding.buttonDivision.setOnClickListener {
+            binding.input.text = addToInputText("÷")
+        }
+        binding.buttonEquals.setOnClickListener {
+            showResult()
+        }
     }
 
+    private  fun getInputExpression(): String {
+        var expression = binding.input.text.replace(Regex("÷"),"/")
+        expression = expression.replace(Regex("×"),"*")
+        return expression
+    }
+    private fun showResult() {
+        try {
+            val expression = getInputExpression()
+            val result = Expression(expression).calculate()
+            if (result.isNaN()){
+                binding.output.text = "Error"
+                binding.output.setTextColor(ContextCompat.getColor(this,R.color.red))
+            }else {
+                binding.output.text = DecimalFormat("0.######").format(result).toString()
+                binding.output.setTextColor(ContextCompat.getColor(this, R.color.green))
+            }
+        }catch (e : Exception){
+            binding.output.text = "Error"
+            binding.output.setTextColor(ContextCompat.getColor(this, R.color.red))
+        }
+    }
+
+    private fun addToInputText(buttonValue: String): String {
+        return "${binding.input.text}$buttonValue"
+    }
 }
